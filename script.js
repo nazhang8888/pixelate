7fd442637dd8c1467e23d191b452bec7f36900f5
nazhang8888 <nazhang@pm.me>
2023-03-06 22:08:44 -0500

    class work

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 let chosenColor = "red";
 const table = document.getElementsByTagName("table")[0];
 const addRowButton = document.getElementById("add-row");
-const select = document.getElementsByTagName("select");
+const selectElement = document.querySelector("#select");
 
 function makeRow() {
   const row = document.createElement("tr");
@@ -19,15 +19,26 @@ addRowButton.addEventListener("click", makeRow);
 
 function colorize(event) {
   const target = event.target;
-  if (target.className.length) {
-    target.className = "";
-  } else {
-    target.className = chosenColor;
+  if (target.tagName === "td") {
+    if (target.className === chosenColor) {
+      target.className = "";
+    } else {
+      target.className = chosenColor;
+    }
   }
+
+  // switch (target.className) {
+  //   case target.className === "blue":
+  //     target.className = chosenColor;
+  //     break;
+
+  //   default:
+  //     target.className = "";
+  // }
 }
 
 table.addEventListener("click", colorize);
 
-select.addEventListener("change", function (event) {
-  chosenColor = event.target.className;
+selectElement.addEventListener("change", (event) => {
+  chosenColor = event.target.value;
 });
